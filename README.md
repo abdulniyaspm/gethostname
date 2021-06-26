86f2c264aed67386b4c7a099e5c047f871817042
abdulniyaspm <abdulniyaspm@gmail.com>
2021-06-26 23:10:48 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# gethostname
+
+A rust program which returns name of the localhost.
+
+This is just a learning project (Especially to learn how to consume platform APIs in Rust). Thanks to [libc](https://github.com/rust-lang/libc) :)
+
+## Meta
+
+Abdul Niyas P M – [@AbdulNiyas19](https://twitter.com/AbdulNiyas19) – abdulniyaspm@gmail.com
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::ffi::CStr;
+use std::io::Error;
+
+use libc;
+
+const HOSTNAME_BUFF_SIZE: usize = 1024;
+
+fn main() {
+    let mut x: Vec<i8> = Vec::with_capacity(HOSTNAME_BUFF_SIZE);
+    let x_ptr = x.as_mut_ptr();
+    unsafe {
+        let returncode = libc::gethostname(x_ptr, HOSTNAME_BUFF_SIZE);
+        if returncode == -1 {
+            println!(
+                "gethostname function call failed with error: {:?}",
+                Error::last_os_error()
+            );
+        }
+        let cstr = CStr::from_ptr(x_ptr);
+        println!("{}", cstr.to_str().unwrap());
+    }
+}
