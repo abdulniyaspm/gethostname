5df4467f32d13b3dc01192a0cec53b4e4a96b065
abdulniyaspm <abdulniyaspm@gmail.com>
2021-06-27 11:36:36 +0530

    names updated

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ use libc;
 const HOSTNAME_BUFFER_SIZE: usize = 1024;
 
 fn main() {
-    let mut x: Vec<i8> = Vec::with_capacity(HOSTNAME_BUFFER_SIZE);
-    let x_ptr = x.as_mut_ptr();
+    let mut buffer: Vec<i8> = Vec::with_capacity(HOSTNAME_BUFFER_SIZE);
+    let buffer_ptr = buffer.as_mut_ptr();
     unsafe {
-        let returncode = libc::gethostname(x_ptr, HOSTNAME_BUFFER_SIZE);
+        let returncode = libc::gethostname(buffer_ptr, HOSTNAME_BUFFER_SIZE);
         if returncode == -1 {
             println!(
-                "gethostname function call failed with error: {:?}",
+                "gethostname system call failed with error: {:?}",
                 Error::last_os_error()
             );
         }
-        let cstr = CStr::from_ptr(x_ptr);
+        let cstr = CStr::from_ptr(buffer_ptr);
         println!("{}", cstr.to_str().unwrap());
     }
 }
