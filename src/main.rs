a7ddeba7b0626017bec45b32a0f685e4e0a7500f
abdulniyaspm <abdulniyaspm@gmail.com>
2021-07-03 15:51:17 +0530

    else added

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,18 @@ fn main() {
     let mut buffer: Vec<i8> = Vec::with_capacity(HOSTNAME_BUFFER_SIZE);
     let buffer_ptr = buffer.as_mut_ptr();
     unsafe {
-        let returncode = libc::gethostname(buffer_ptr, HOSTNAME_BUFFER_SIZE);
-        if returncode == -1 {
+        let returncode = libc::gethostname(
+            buffer_ptr as *mut libc::c_char,
+            HOSTNAME_BUFFER_SIZE as libc::size_t,
+        );
+        if returncode < 0 {
             println!(
                 "gethostname system call failed with error: {:?}",
                 Error::last_os_error()
             );
+        } else {
+            let cstr = CStr::from_ptr(buffer_ptr);
+            println!("{}", cstr.to_str().unwrap());
         }
-        let cstr = CStr::from_ptr(buffer_ptr);
-        println!("{}", cstr.to_str().unwrap());
     }
 }
